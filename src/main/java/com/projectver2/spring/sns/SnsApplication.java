307befe279ea8463437a37e9ea57fd2b1dbe7558
d982h8st7 <sinnarver15@gmail.com>
2023-08-21 23:58:46 +0900

    base code

---
+++ src/main/java/com/projectver2/spring/sns/SnsApplication.java
@@ -0,0 +1,14 @@
+package com.projectver2.spring.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SnsApplication.class, args);
+    }
+
+}
