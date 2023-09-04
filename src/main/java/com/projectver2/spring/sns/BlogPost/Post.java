203bc32412c5820be38cfba7da840b9769b06c65
tomilriddle <131672970+tomilriddle@users.noreply.github.com>
2023-09-05 00:34:36 +0900

    post create 200 연결 성공

--- src/main/java/com/projectver2/spring/sns/BlogPost/Post.java
+++ src/main/java/com/projectver2/spring/sns/BlogPost/Post.java
@@ -38,4 +38,3 @@ public class Post extends BaseTimeEntity {
 
     }
 }
-
--- src/main/java/com/projectver2/spring/sns/BlogPost/PostService.java
+++ src/main/java/com/projectver2/spring/sns/BlogPost/PostService.java
@@ -15,28 +15,28 @@ public class PostService {
         return postRepository.save(requestDto.toEntity());
     }
 
-    public String update(Integer id, PostRequestDto requestDto) {
-        String message = "fail";
-        Post post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
-
-        // 작성자만 수정 가능
-        if (postRepository.findById(id).get().getUser().getId().equals(requestDto.getUser().getId())) {
-            post.update(requestDto.getContent());
-            message = "success";
-        }
-
-        return message;
-
-        public String delete(Integer id, User user) {
-            String message = "fail";
-            Post post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
-
-            if (post.getUser()().getId().equals(user.getId())) { // 작성자만 삭제 가능
-                postRepository.delete(post);
-                message = "success";
-            }
-
-            return message;
-
-    }
+//    public String update(Integer id, PostRequestDto requestDto) {
+//        String message = "fail";
+//        Post post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+//
+//        // 작성자만 수정 가능
+//        if (postRepository.findById(id).get().getUser().getId().equals(requestDto.getUser().getId())) {
+//            post.update(requestDto.getContent());
+//            message = "success";
+//        }
+//
+//        return message;
+//
+//        public String delete(Integer id, User user) {
+//            String message = "fail";
+//            Post post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+//
+//            if (post.getUser()().getId().equals(user.getId())) { // 작성자만 삭제 가능
+//                postRepository.delete(post);
+//                message = "success";
+//            }
+//
+//            return message;
+//
+//    }
 }
