276fdb25012f5559c85df52800d4edf6ea88f3d4
tomilriddle <131672970+tomilriddle@users.noreply.github.com>
2023-09-07 03:20:39 +0900

    CRUD 구현

--- src/main/java/com/projectver2/spring/sns/BlogPost/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.projectver2.spring.sns.BlogPost;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
