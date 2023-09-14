ed6beaf20c7964f8259d3f6234662d3f8db2d217
wsb347 <wsb347@naver.com>
2023-09-14 19:14:11 +0900

    임시저장

--- src/main/java/com/projectver2/spring/sns/mypage/repository/PostListRepository.java
+++ src/main/java/com/projectver2/spring/sns/mypage/repository/PostListRepository.java
@@ -2,6 +2,7 @@ package com.projectver2.spring.sns.mypage.repository;
 
 import com.projectver2.spring.sns.BlogPost.Post;
 import com.projectver2.spring.sns.mypage.entity.Bookmark;
+import com.projectver2.spring.sns.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,5 +15,6 @@ public interface PostListRepository extends JpaRepository<Post, Integer> {
 
      List<Post> findPostByUser(String user);
 
-
+     @Query("SELECT p FROM post p inner JOIN user u ON p.user = u.userid WHERE u.userid = :userid")
+     List<Post> findPostByUserandlike(@Param("userid") User user, Post post);
 }
